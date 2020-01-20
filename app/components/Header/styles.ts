75cb502373737d8cb88891fbe9815622df4dfc67
Mugisha Caleb <mcaleb808@gmail.com>
2020-01-20 20:19:19 +0100

    add settings components

---
+++ app/components/Header/styles.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    left: 1,
+    right: 0,
+    top: 1,
+    paddingTop: 25
+  },
+  button: {
+    alignSelf: 'flex-end',
+    paddingVertical: 5,
+    paddingHorizontal: 20
+  },
+  icon: {
+    width: 22
+  }
+});
+
+export default styles;
